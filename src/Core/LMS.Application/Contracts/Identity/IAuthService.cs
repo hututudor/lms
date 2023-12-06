31ac11dea448d54c2b89294a035c5852d497f80d
Tudor Hutu <hutu.tudor@yahoo.com>
2023-12-06 23:03:10 +0200

    add identity package

---
+++ src/Core/LMS.Application/Contracts/Identity/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using LMS.Application.Models.Identity;
+
+namespace LMS.Application.Contracts.Identity
+{
+    public interface IAuthService
+    {
+        Task<(int, string)> Registeration(RegistrationModel model, string role);
+        Task<(int, string)> Login(LoginModel model);
+    }
+}
---
+++ src/LMS.Identity/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LMS.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Identity
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     optionsBuilder.UseNpgsql("Host=localhost;Database=lmsUsers;Username=postgres;Password=root");
+        // }
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
+ 
